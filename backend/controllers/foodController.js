c62cc786c55ce832058444056d9e715e57e6bb9f
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-10-31 10:23:53 +0530

    Create api endpoint for foods

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,13 @@
+import exp from "constants";
+import foodModel from "../models/foodModels.js";
+import fs from 'fs'
+
+// add food items
+
+const addFood = async (req,res) => {
+    let image_filename = `${req.file.filename}`
+}
+
+export {
+    addFood,
+}
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+
+// env variable connection
+dotenv.config()
+//console.log("Database URL", process.env.DB_USERNAME);
+// console.log("Database Password", process.env.DB_PASSWORD);
+
+
+// app configuration
+const app = express()
+const port = 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//db connection
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRouter)
+
+app.get("/", (req,res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
