253986704d60dbf42b3ccf1c4ee73f15ed80fa65
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-12-27 14:59:59 +0530

    Minor UI fixes in admin side on main branch

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { assets } from "../../assets/frontend_assets/assets";
 
 function Contact() {
@@ -22,11 +22,11 @@ function Contact() {
     }).then((res) => res.json());
 
     if (res.success) {
-        Swal.fire({
-            title: "Success!",
-            text: "Respnse recieved, will reach out to you soon",
-            icon: "success"
-          });
+      Swal.fire({
+        title: "Success!",
+        text: "Respnse recieved, will reach out to you soon",
+        icon: "success",
+      });
     }
   };
 
@@ -36,42 +36,42 @@ function Contact() {
         className="contact-banner bg-cover bg-center h-[50vh] flex flex-col mb-20 justify-center items-center text-white"
         style={{ backgroundImage: `url(${assets.contact})` }}
       >
-        <h1 className="text-4xl md:text-7xl font-bold mb-4 font-expletus">
+        <h1 className="mb-4 text-4xl font-bold md:text-7xl font-expletus">
           Contact Us
         </h1>
-        <h2 className="text-3xl md:text-5xl font-semibold mb-4 p-2">
+        <h2 className="p-2 mb-4 text-3xl font-semibold md:text-5xl">
           Get answers to your queries ASAP!
         </h2>
       </div>
 
-      <div className="contact-card grid grid-cols-1 md:grid-cols-3 gap-10 w-full max-w-7xl mx-auto mt-5 mb-28">
-        <div className="flex items-center justify-center flex-col bg-white drop-shadow rounded-xl p-4 hover:scale-105 transition-all duration-500">
-          <img src={assets.telephone} alt="" className=" w-20 h-20 mb-5" />
-          <h2 className="text-2xl font-medium mb-2">Hotline</h2>
+      <div className="grid w-full grid-cols-1 gap-10 mx-auto mt-5 contact-card md:grid-cols-3 max-w-7xl mb-28">
+        <div className="flex flex-col items-center justify-center p-4 transition-all duration-500 bg-white drop-shadow rounded-xl hover:scale-105">
+          <img src={assets.telephone} alt="" className="w-20 h-20 mb-5 " />
+          <h2 className="mb-2 text-2xl font-medium">Hotline</h2>
           <p className="text-base">+91 1234567890</p>
         </div>
 
-        <div className="flex items-center justify-center flex-col bg-white drop-shadow rounded-xl p-4 hover:scale-105 transition-all duration-500">
-          <img src={assets.map} alt="" className=" w-20 h-20 mb-5" />
-          <h2 className="text-2xl font-medium mb-2">Our Location</h2>
-          <p className="text-base px-6">
+        <div className="flex flex-col items-center justify-center p-4 transition-all duration-500 bg-white drop-shadow rounded-xl hover:scale-105">
+          <img src={assets.map} alt="" className="w-20 h-20 mb-5 " />
+          <h2 className="mb-2 text-2xl font-medium">Our Location</h2>
+          <p className="px-6 text-base">
             55 Main Street, The Grand Avenue 2nd Block, New York City
           </p>
         </div>
 
-        <div className="flex items-center justify-center flex-col bg-white drop-shadow rounded-xl p-4 hover:scale-105 transition-all duration-500">
-          <img src={assets.email} alt="" className=" w-20 h-20 mb-5" />
-          <h2 className="text-2xl font-medium mb-2">Official Mail</h2>
+        <div className="flex flex-col items-center justify-center p-4 transition-all duration-500 bg-white drop-shadow rounded-xl hover:scale-105">
+          <img src={assets.email} alt="" className="w-20 h-20 mb-5 " />
+          <h2 className="mb-2 text-2xl font-medium">Official Mail</h2>
           <p className="text-base">meal_matrix@gmail.com</p>
         </div>
       </div>
 
-      <div className="contact-form flex justify-center items-center mb-20">
-        <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg p-8">
-          <h2 className="text-center text-sm font-semibold text-gray-500 mb-1">
+      <div className="flex items-center justify-center mb-20 contact-form">
+        <div className="w-full max-w-4xl p-8 bg-white rounded-lg shadow-lg">
+          <h2 className="mb-1 text-sm font-semibold text-center text-gray-500">
             KEEP IN TOUCH
           </h2>
-          <h1 className="text-center text-4xl font-semibold font-expletus text-gray-800 mb-6">
+          <h1 className="mb-6 text-4xl font-semibold text-center text-gray-800 font-expletus">
             Send us a Message
           </h1>
           <form className="space-y-4" onSubmit={onSubmit}>
@@ -104,13 +104,13 @@ function Contact() {
               <textarea
                 placeholder="Your Message *"
                 name="message"
-                className="w-full p-3 h-32 bg-gray-100 border border-gray-200 rounded-md resize-none focus:outline-none focus:border-gray-300"
+                className="w-full h-32 p-3 bg-gray-100 border border-gray-200 rounded-md resize-none focus:outline-none focus:border-gray-300"
                 required
               ></textarea>
             </div>
             <button
               type="submit"
-              className="py-3 px-7 bg-primary text-lg text-white font-semibold rounded-md flex justify-center items-center space-x-2 hover:text-primary hover:bg-secondary transition-all duration-500"
+              className="flex items-center justify-center py-3 space-x-2 text-lg font-semibold text-white transition-all duration-500 rounded-md px-7 bg-primary hover:text-primary hover:bg-secondary"
             >
               <span>
                 <i className="ri-send-plane-line"></i>
