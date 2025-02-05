0de154210280afdc40a8c61a598edde5e02b5123
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-02-05 15:37:55 +0530

    Minor UI changes

--- admin/src/components/Navbaar/Navbar.jsx
+++ admin/src/components/Navbaar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { assets } from '../../assets/admin_assets/assets'
 
 function Navbar() {
+
+  const navigate = useNavigate()
+  
   return (
     <div className='flex justify-between items-center py-4 px-[4%]'>
-        <img src={assets.logo} alt="" className='w-42'/>
+        <img src={assets.logo} alt="" className='cursor-pointer w-42' onClick={() => navigate("/")}/>
         <img src={assets.profile_image} alt="" className='w-[50px]'/>
     </div>
   )
